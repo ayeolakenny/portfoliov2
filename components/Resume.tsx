2cc58138a00096ae9b90ed0a146cb022ad187a64
ayeolakenny <ayeolakenny@gmail.com>
2021-04-16 20:59:18 +0100

    bug('fixing images issue on prod')

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -8,10 +8,14 @@ const Resume: React.FC<{}> = ({}) => (
         <div className="">
           <div className="">
             <div className="">
-              <h2 className="text-white text-5xl">Check out my résumé!</h2>
-              <button className="mt-8 px-14 py-3.5 bg-transparent border-double border-4 border-white bg-gray-600 rounded text-white font-bold tracking-wide shadow-xl hover:bg-gray-800 hover:shadow-md text-xl">
+              <h2 className="text-white text-5xl mb-7">Check out my résumé!</h2>
+              <a
+                href="/resume2021.pdf"
+                target="_blank"
+                className="mt-8 px-14 py-3.5 bg-transparent border-double border-4 border-white bg-gray-600 rounded text-white font-bold tracking-wide shadow-xl hover:bg-gray-800 hover:shadow-md text-xl"
+              >
                 Grab A Copy
-              </button>
+              </a>
             </div>
           </div>
         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,5 +4,18 @@ import "tailwindcss/tailwind.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+//  *********************************************
+//   _   _              _   _                   _
+//  | | | | ___ _   _  | |_| |__   ___ _ __ ___| |
+//  | |_| |/ _ \ | | | | __| '_ \ / _ \ '__/ _ \ |
+//  |  _  |  __/ |_| | | |_| | | |  __/ | |  __/_|
+//  |_| |_|\___|\__, |  \__|_| |_|\___|_|  \___(_)
+//              |___/
+//  Lookin' at the source code huh?
+//  Be sure to drop me a line! I'd love to chat,
+//  whether it's about code, casual or anything
+//  coffee, snowboarding, or anything in between.
+//  - Kehinde
+//  **********************************************
 
 export default MyApp;
